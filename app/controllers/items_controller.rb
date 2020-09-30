8cff5b19c713e8a41e9c8bfa8978f1096a472621
shu1124 <shu1124@users.noreply.github.com>
2020-09-30 14:04:51 +0900

    出品機能追加２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  
+  private
+    def items_params
+      params.require(:item).permit(:name,:text,:fee,:user,:category_id,:status_id,
+        :delivery_fee_id,:delivery_date_id,:prefectures_id,:image).merge(user_id: current_user.id)
+    end
+
 end
