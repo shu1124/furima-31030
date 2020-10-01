7161905172f3826f1db6df418893d8bfc139aed8
shu1124 <shu1124@users.noreply.github.com>
2020-10-01 17:55:47 +0900

    コントローラーのリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == Item.find(params[:id]).user_id
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
   end
 
   def items_params
